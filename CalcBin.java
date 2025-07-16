6123827f49de07ae76fb05ad75d36f83cbdbe483
Diogo Lima <diogololiveira13@gmail.com>
2025-07-16 12:22:46 -0300

    Criação da transformaçãp e do agrupamento
    Transforma em binario
    agrupa em seus respectivos grupos, formando uma matriz

--- CalcBin.java
+++ CalcBin.java
@@ -1,12 +1,36 @@
 public class CalcBin{
 	
-	static public int [] transformaBin (int numVdd []);
-	tamLista = numVdd[].length;
 	
+	
+	static public String [] transformaBin (int numVdd []){
+	int tamLista = numVdd.length;
+	String [] bin = new String [tamLista];
 	for (int i=0; i <tamLista; i++){
+		bin[i] = Integer.toBinaryString(numVdd[i]);
 		
 	}
+	return bin;
+}
+
+	static public String[][] agrupaGrupos(String bin[]){
+	int tamLista = bin.length;
+	int tamBitsTotal = bin[tamLista-1].length();
+	int grupoDetect, tamBits;
+	String[][] grupoList = new String [tamLista] [tamBitsTotal+1];
 	
+	for (int i =0; i< tamLista; i++){
+		grupoDetect=0;
+		tamBits = bin[i].length();
+		for (int j=0; j< tamBits; j++){
+		if (bin[i].charAt(j) == '1'){
+			grupoDetect++;
+		}
+		grupoList[i][grupoDetect] = bin[i];
+		}	
+	}	
+		return grupoList;
+	}
 	
+
 }
 
